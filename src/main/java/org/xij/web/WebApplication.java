a3a269eb3bba0854177b146c71406a58eaa4e766
JeSon-wht <dengxinxin@zhaozhi.com>
2019-07-12 16:45:52 +0800

    f

--- src/main/java/org/xij/web/WebApplication.java
+++ src/main/java/org/xij/web/WebApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.xij.annotation.MybatisMapper;
 
+
 @SpringBootApplication(scanBasePackages = {"org.xij.web"})
 @MapperScan(annotationClass = MybatisMapper.class, basePackages = {"org.xij.web.module"})
 public class WebApplication {
